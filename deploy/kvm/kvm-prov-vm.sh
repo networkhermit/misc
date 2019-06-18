7286d3ca471e217e27f0b7453b6e1134b7ab1b30
vac <dot.fun@protonmail.com>
2019-06-18 13:28:09 +0000

    This is where it all begins...

--- deploy/kvm/kvm-prov-vm.sh
+++ deploy/kvm/kvm-prov-vm.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 if (( EUID != 0 )); then
     echo "✗ This script must be run as root" 1>&2
     exit 1
 fi
 
-if (( $# != 2 )); then
+ARGC=4
+LEAST_ARGC=2
+
+if (( $# > ARGC )); then
+    echo "✗ number of arguments exceeds limit: ‘${#}/${ARGC}’" 1>&2
+    exit 1
+elif (( $# < LEAST_ARGC )); then
+    echo "✗ too few arguments: ‘${#}/${LEAST_ARGC}’" 1>&2
     exit 1
 fi
+
+DISTRO=${1}
+
+NAME=${2}
+CPU=${3:-4}
+MEMORY=${4:-8192}
 
 EXTRA_ARGUMENT=()
 
 INSTALLER_PARAMETER=(--extra-args 'console=ttyS0,115200n8 nameserver=1.0.0.1')
 
-case "${1}" in
+case "${DISTRO}" in
     arch)
         IMAGE=$(find images/ -type f -name 'archlinux-*-x86_64.iso' | sort --version-sort | tail --lines 1)
 
@@ -36,23 +53,24 @@ case "${1}" in
         EXTRA_ARGUMENT+=(--os-variant ubuntu18.04)
         ;;
     *)
-        echo "✗ unknown distro: ‘${1}’" 1>&2
+        echo "✗ unknown distro: ‘${DISTRO}’" 1>&2
         exit
         ;;
 esac
 
 virt-install \
     --connect qemu:///system \
-    --console pty,target_type=serial \
+    --console char_type=pty,target_type=serial \
     --cpu host \
-    --disk format=qcow2,path="/var/local/images/${2}.img",size=20 \
+    --disk device=disk,format=qcow2,path="/var/local/images/${NAME}.img",size=40 \
     --features kvm_hidden=off \
     --graphics none \
     --hvm \
-    --memory 2048 \
-    --name "${2}" \
-    --network network=default \
+    --memory "${MEMORY}" \
+    --name "${NAME}" \
+    --network model=virtio,network=default \
     --os-type linux \
-    --vcpus 2,maxvcpus=4 \
+    --vcpus cpuset=auto,vcpus="${CPU}" \
     --virt-type kvm \
+    --autostart \
     "${EXTRA_ARGUMENT[@]}"
