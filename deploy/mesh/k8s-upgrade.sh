4d9470a2b061b981f71b9ffbd63d78b323d4fa5c
vac <dot.fun@protonmail.com>
2019-07-09 14:09:59 +0000

    This is where it all begins...

--- deploy/mesh/k8s-upgrade.sh
+++ deploy/mesh/k8s-upgrade.sh
@@ -12,7 +12,7 @@ sudo apt install --allow-change-held-packages --assume-yes kubeadm${K8S_PKG_VER:
 sudo apt-mark hold kubeadm
 kubeadm config images list --kubernetes-version "$(kubeadm version --output short)"
 
-# control plane leader only
+# primary control plane only
 sudo kubeadm upgrade plan
 kubeadm config view | sudo kubeadm upgrade diff "$(kubeadm version --output short)" --config /dev/stdin
 sudo kubeadm upgrade apply "$(kubeadm version --output short)" --dry-run
@@ -21,13 +21,13 @@ sudo kubeadm upgrade apply "$(kubeadm version --output short)" --yes
 sudo apt install --allow-change-held-packages --assume-yes kubectl${K8S_PKG_VER:+=${K8S_PKG_VER[kubectl]}} < /dev/null
 sudo apt-mark hold kubectl
 
-# control plane leader only
+# primary control plane only
 kubectl --namespace kube-system delete daemonsets kube-proxy
 
 kubectl drain "${HOSTNAME,,}" --delete-local-data --ignore-daemonsets
 sudo apt install --allow-change-held-packages --assume-yes kubelet${K8S_PKG_VER:+=${K8S_PKG_VER[kubelet]}} < /dev/null
 sudo apt-mark hold kubelet
-# [control plane] [worker]
+# [replica control plane] [worker]
 sudo kubeadm upgrade node --kubelet-version "$(kubelet --version | cut --delimiter ' ' --fields 2)"
 sudo systemctl restart kubelet.service
 kubectl uncordon "${HOSTNAME,,}"
@@ -37,7 +37,7 @@ kubectl cluster-info
 kubectl get nodes --output wide
 kubectl get pods --all-namespaces --output wide
 
-# control plane leader only
+# primary control plane only
 sudo rm --force --recursive --verbose /etc/kubernetes/tmp
 
 sudo docker image ls --format '{{.Repository}}:{{.Tag}}' | grep --word-regexp 'k8s.gcr.io' | sort
--- deploy/os/installation-guide.md
+++ deploy/os/installation-guide.md
@@ -535,7 +535,7 @@ sudo rm --force --verbose kali-archive-keyring_*_all.deb
 # change distro source
 
 # make distro sync
-sudo apt install --assume-yes kali-{defaults,linux-full} < /dev/null
+sudo apt install --assume-yes kali-{defaults,linux-default} < /dev/null
 
 # update message of the day
 
