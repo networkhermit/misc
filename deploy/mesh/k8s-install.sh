2380498a027643a1ec050fad0746958a21a12f26
vac <dot.fun@protonmail.com>
2020-05-02 09:32:24 +0000

    This is where it all begins...

--- deploy/mesh/k8s-install.sh
+++ deploy/mesh/k8s-install.sh
@@ -64,7 +64,7 @@ kubectl --namespace kube-system delete daemonsets kube-proxy
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # deploy kubernetes dashboard
-kubectl apply --filename 'https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml'
+kubectl apply --filename 'https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml'
 kubectl create serviceaccount cluster-admin-dashboard --namespace kube-system
 kubectl create clusterrolebinding cluster-admin-dashboard --clusterrole cluster-admin --serviceaccount kube-system:cluster-admin-dashboard
 
